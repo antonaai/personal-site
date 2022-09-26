117c7d93b4fcc4674cd210c20746dd04184b5965
Antonio Aiello <aaiello.ext@baybridgedigital.com>
2022-09-26 09:44:28 +0200

    add comment

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+// http://localhost:3000
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.redirect('/en')
